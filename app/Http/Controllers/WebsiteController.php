8b8a7cc07a9bf9a8f076c0bc8c2e9e4c0a5e264a
Richard Joseph Porter <rjosephporter@gmail.com>
2021-10-14 21:28:45 +0800

    Initial Commit

---
+++ app/Http/Controllers/WebsiteController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\UserSubscribedToWebsite;
+use App\Http\Requests\SubscribeRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Models\Website;
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Website $website)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Website $website)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Website $website)
+    {
+        //
+    }
+
+    public function subscribe(SubscribeRequest $request, Website $website)
+    {
+        $user = User::findOrFail($request->user_id);
+        $website->users()->syncWithoutDetaching($user);
+
+        return new UserResource($user);
+    }
+}
---
+++ app/Http/Controllers/WebsitePostController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use App\Models\Website;
+use Illuminate\Http\Request;
+
+class WebsitePostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return PostResource
+     */
+    public function store(PostRequest $request, Website $website)
+    {
+        return new PostResource(
+            $website->posts()->create($request->only(['title', 'description']))
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
